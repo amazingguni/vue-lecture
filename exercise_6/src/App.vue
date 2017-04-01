876608fd66f4be95894d3e702d8cff34458fff25
amazingguni <amazingguni@gmail.com>
2017-04-01 18:53:53 +0900

    8장 진행중

---
+++ exercise_6/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <home></home>
+</template>
+
+<script>
+import Home from './Home.vue';
+
+export default {
+  components:{
+    'home': Home,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ exercise_6/src/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <home-header></home-header>
+        <hr>
+        <server-detail></server-detail>
+        <hr>
+        <home-footer></home-footer>
+    </div>
+</template>
+
+<script>
+import HomeHeader from './HomeHeader.vue';
+import ServerDetail from './ServerDetail.vue';
+import HomeFooter from './HomeFooter.vue';
+
+export default {
+  components:{
+    'home-header': HomeHeader,
+    'server-detail': ServerDetail,
+    'home-footer': HomeFooter,
+  }
+}
+</script>
+
+<style>
+</style>
