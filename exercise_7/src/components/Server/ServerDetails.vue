380bfb316685e3d6dc9e1e805c5db923b087c365
amazingguni <amazingguni@gmail.com>
2017-04-02 13:13:45 +0900

    Finish my exercise 7

--- exercise_7/src/components/Server/ServerDetails.vue
+++ exercise_7/src/components/Server/ServerDetails.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+      <template v-if="server">
+        <p>Server #{{ server.id }} : {{ server.status }}</p>
+        <button @click="setNormal">Set server status to Normal</button>
+      </template>
+      <p v-else>Server Details are currently not updated</p>
     </div>
 
 </template>
 
 <script>
+  import { eventBus } from '../../main.js'
+  export default {
+    data(){
+      return {
+        server:null
+      }
+    },
+    methods:{
+      setNormal: function(){
+        eventBus.setNormal(this.server.id);
+      }
+    },
+    created(){
+      eventBus.$on('serverSelected', (server) => {
+        this.server = server;
+      });
+
+    }
+  }
 </script>
 
 <style>
--- exercise_7/src/components/Server/Servers.vue
+++ exercise_7/src/components/Server/Servers.vue
@@ -1,16 +1,47 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
-        </ul>
-    </div>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="server in servers"
+        @click="selectServer(server)">
+          Server #{{ server.id }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import { eventBus } from '../../main.js'
+  export default {
+    data(){
+      return {
+        servers:[
+          {id:1, status:"Critical"},
+          {id:2, status:"Critical"},
+          {id:3, status:"Unknown"},
+          {id:4, status:"Normal"},
+          {id:5, status:"Normal"},
+        ]
+      }
+    },
+    methods:{
+      selectServer(server){
+        eventBus.selectServer(server);
+      }
+    },
+    created(){
+      eventBus.$on('statusChanged', (id) => {
+        for(var i = 0 ; i< this.servers.length; i++){
+          var server = this.servers[i];
+          if (server.id === id){
+            server.status = "Normal";
+          }
+        }
+      });
+    }
+
+  }
 </script>
 
 <style>
