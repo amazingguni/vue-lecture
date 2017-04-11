210b1fe2a01dc3f831760077e6c95810f9de53fd
amazingguni <amazingguni@gmail.com>
2017-04-11 22:30:23 +0900

    과제 9 내 답안

---
+++ exercise_9/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="container">
+        <form v-if="!isSubmitted">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                  <app-fullname v-model="userData.fullName"></app-fullname>
+
+                  <div class="form-group">
+                      <label for="email">Email</label>
+                      <input
+                        type="text"
+                        id="email"
+                        class="form-control"
+                        v-model="userData.email">
+                  </div>
+
+                  <div class="form-group">
+                      <label for="password">Password</label>
+                      <input
+                        type="password"
+                        id="password"
+                        class="form-control"
+                        v-model="userData.password"
+                        >
+                  </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="yes">
+                        <input
+                            type="radio"
+                            id="yes"
+                            value="yes"
+                            v-model="storedata"> Yes
+                    </label>
+                    <label for="no">
+                        <input
+                            type="radio"
+                            id="no"
+                            value="no"
+                            v-model="storedata"> No
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ userData.fullName }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ storedata }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import FullName from './FullName.vue';
+    export default {
+      data(){
+        return {
+          userData:{
+            fullName: '',
+            email:'',
+            password: '',
+          },
+          isSubmitted : false,
+          storedata: 'no',
+        };
+      },
+      methods : {
+        submitted(){
+          this.isSubmitted = true;
+        }
+      },
+      components:{
+        appFullname: FullName
+      }
+
+    }
+</script>
+
+<style>
+</style>
---
+++ exercise_9/src/FullName.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="form-group">
+      <label for="firstName">FirstName</label>
+      <input
+        type="text"
+        id="firstName"
+        class="form-control"
+        v-model="firstName">
+      <label for="lastName">LastName</label>
+      <input
+        type="text"
+        id="lastName"
+        class="form-control"
+        v-model="lastName">
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return {
+              firstName:'',
+              lastName:'',
+          }
+        },
+        watch:{
+          firstName:function(){
+            this.nameChanged();
+          },
+          lastName:function(){
+            this.nameChanged();
+          }
+        },
+        computed:{
+          fullName: function(){
+            return this.firstName + ' ' + this.lastName;
+          }
+        },
+        methods:{
+          nameChanged(){
+            this.$emit('input', this.fullName);
+          }
+        },
+        props: ['value'],
+    }
+</script>
+
+<style>
+
+</style>
