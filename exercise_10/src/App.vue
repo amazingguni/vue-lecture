bb245e6d5691e70552e6e462426b7c5318a97df7
amazingguni <amazingguni@gmail.com>
2017-04-12 23:43:19 +0900

    숙제 10번 완료

---
+++ exercise_10/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 v-myon:click="showAlert">Directives Exercise</h1>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      methods:{
+        showAlert:function(){
+          alert('Alert!!!');
+        }
+      },
+      directives: {
+        myon: {
+          bind(el, binding, vnode){
+            if (binding.arg == 'click'){
+              el.onclick=function(){
+                binding.value()
+              }
+            }
+          }
+        }
+      }
+    }
+</script>
+
+<style>
+</style>
