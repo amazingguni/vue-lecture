7cf265d40019e01307a92d3ed3257873e7ae968b
amazingguni <amazingguni@gmail.com>
2017-04-15 13:04:27 +0900

    14장 절반 끝내고 퀴즈게임 프로젝트 초기 세팅 완료

--- section_14/src/App.vue
+++ section_14/src/App.vue
@@ -2,13 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-                <hr>
-                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
-                <br><br>
-                <transition name="fade">
-                  <div class="alert alert-info" v-if="show">This is some info</div>
-                </transition>
+                <h1 class="text-center">The Super Quiz</h1>
             </div>
         </div>
     </div>
@@ -18,26 +12,7 @@
     export default {
         data() {
             return {
-              show: false,
             }
-        }
+        },
     }
 </script>
-
-<style>
-  .fade-enter{
-
-  }
-
-  .fade-enter-active{
-
-  }
-
-  .fade-leave{
-
-  }
-
-  .fade-leave-active{
-
-  }
-</style>
