b0f55e001324c15c19d3fbf327f5a3e6e9fcf6e5
amazingguni <amazingguni@gmail.com>
2017-04-22 18:45:03 +0900

    ㅇ16장 거의 끝, 디렉토리 구조 수정

--- session_16/src/routes.js
+++
@@ -1,8 +0,0 @@
-import User from './components/user/User.vue';
-import Home from './components/Home.vue';
-
-
-export const routes = [
-  { path: '/', component: Home },
-  { path: '/user', component: User },
-];
