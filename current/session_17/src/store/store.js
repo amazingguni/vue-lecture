c3d53e296173f71c9806d833bd34c81d7b762923
amazingguni <amazingguni@gmail.com>
2017-04-23 16:54:08 +0900

    computed에서 get/set 사용

--- current/session_17/src/store/store.js
+++ current/session_17/src/store/store.js
@@ -6,6 +6,7 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
     counter: 0,
+    value: 0,
   },
   getters: {
     doubleCounter: state => {
@@ -13,14 +14,41 @@ export const store = new Vuex.Store({
     },
     stringCounter: state => {
       return state.counter + ' Clicks';
-    }
+    },
+    value: state => {
+      return state.value;
+    },
   },
   mutations: {
-    increment: state => {
-      state.counter++;
+    increment: (state, payload) => {
+      state.counter += payload;
     },
-    decrement: state => {
-      state.counter--;
+    decrement: (state, payload) => {
+      state.counter -= payload;
+    },
+    updateValue: (state, payload) => {
+      state.value = payload;
+    },
+  },
+  actions: {
+    increment: ({ commit }, payload) => {
+      commit('increment', payload);
+    },
+    decrement: ({ commit }, payload) => {
+      commit('decrement', payload);
+    },
+    asyncIncrement: ({ commit }, payload) => {
+      setTimeout(()=>{
+        commit('increment', payload.by);
+      }, payload.duration);
+    },
+    asyncDecrement: ({ commit }, payload) => {
+      setTimeout(()=>{
+        commit('decrement', payload.by);
+      }, payload.duration);
+    },
+    updateValue({commit}, payload){
+      commit('updateValue', payload);
     },
   }
 });
